0cdabbbcdcb7cc469728eb6414c04699da16b462
Subrat Kumar Behera <118833161+subrat1096@users.noreply.github.com>
2023-06-03 11:26:08 +0530

    display products using javascript

---
+++ app.js
@@ -0,0 +1,73 @@
+// variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart')
+const clearCartBtn = document.querySelector('.clear-cart')
+const cartDOM = document.querySelector('.cart')
+const cartOverlay = document.querySelector('.cart-overlay')
+const cartItems = document.querySelector('.cart-items')
+const cartTotal = document.querySelector('.cart-total')
+const cartContent = document.querySelector('.cart-content')
+const productsDOM = document.querySelector('.products-center')
+
+// cart item
+let cart = []
+
+// getting the products
+class Products{
+    async getProducts(){
+        try{
+            let result = await fetch('products.json')
+            let data = await result.json()
+            let products = data.items
+            products = products.map((item)=>{
+                const {title,price} = item.fields
+                const {id} = item.sys 
+                const image = item.fields.image.fields.file.url
+                return {title,price,id,image}
+            })
+            return products
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+}
+
+// display products
+class UI{
+    displayProducts(products){
+        let result = ''
+        products.forEach((product)=>{
+            result += `<!-- Single Product -->
+            <article class="product">
+              <div class="img-container">
+                <img
+                  src=${product.image}
+                  alt="product"
+                  class="product-img"
+                />
+                <button class="bag-btn" data-id=${product.id}>
+                  <i class="fas fa-shopping-cart"></i>Add to cart
+                </button>
+              </div>
+              <h3>${product.title}</h3>
+              <h4>$${product.price}</h4>
+            </article>
+            <!-- End of Single Product -->`
+        })
+        productsDOM.innerHTML = result
+    }
+}
+
+// local storage
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const ui = new UI()
+    const product = new Products()
+
+    // get all products
+    product.getProducts().then((products)=>{ui.displayProducts(products)})
+})
